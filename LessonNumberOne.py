3c4525eedb3512f9eeb30bfbbe6721defb94762a
Sarah Elizabeth Evanson <sarah_lisbeth@csu.fullerton.edu>
2017-09-28 10:41:59 -0700

    Update LessonNumberOne.py

--- LessonNumberOne.py
+++ LessonNumberOne.py
@@ -38,3 +38,90 @@ NameError: name 'Beautiful_number' is not defined
             ^
 SyntaxError: invalid syntax
 >>>
+>>> print('LOVE',30,82.2)
+LOVE 30 82.2
+>>> print('LOVE'.30,82.2,sep=',')
+  File "<stdin>", line 1
+    print('LOVE'.30,82.2,sep=',')
+                  ^
+SyntaxError: invalid syntax
+>>> print('LOVE', 30, 82.2, sep= ',')
+LOVE,30,82.2
+>>> print('LOVE, 30, 82.2, sep= ', ')
+  File "<stdin>", line 1
+    print('LOVE, 30, 82.2, sep= ', ')
+                                    ^
+SyntaxError: EOL while scanning string literal
+>>> print('LOVE', 30, 82.2, sep= ', ')
+LOVE, 30, 82.2
+>>>>>> decimal = input("give me a decimal value")
+give me a decimal value12.5
+>>> print(decimal)
+12.5
+>>> randomness = input("input randomness: ")
+input randomness: wjehfwuepu2h
+>>> print(randomness)
+wjehfwuepu2h
+>>> print(randomness + " heyyy")
+wjehfwuepu2h heyyy
+>>> print(randomness + beautiful_number +decimal)
+wjehfwuepu2h2512.5
+>>> print(randomness - decimal)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: unsupported operand type(s) for -: 'str' and 'str'
+>>> print(random * decimal)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'random' is not defined
+>>> print(randomness * decimal)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: can't multiply sequence by non-int of type 'str'
+>>> print(randomness * beautiful_number)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: can't multiply sequence by non-int of type 'str'
+>>>
+>>> for i in "python": print(i)
+...
+p
+y
+t
+h
+o
+n
+>>> for i in "python":
+... print(i)
+  File "<stdin>", line 2
+    print(i)
+        ^
+IndentationError: expected an indented block
+>>> for i in "python":
+...     print(i)
+...
+p
+y
+t
+h
+o
+n
+>>> for I in "Sarah":
+...     print(i)
+...
+n
+n
+n
+n
+n
+>>> for I in "Sarah":
+...     print(I)
+...
+S
+a
+r
+a
+h
+>>> print("hey", 'ho', "let's go", sep=',', end='!!\n')
+hey,ho,let's go!!
+>>>
