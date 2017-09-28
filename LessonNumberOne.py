80e9896ee419f9fe8fd8b38914f9d47e30c1c2db
Sarah Elizabeth Evanson <sarah_lisbeth@csu.fullerton.edu>
2017-09-28 10:29:39 -0700

    Create LessonNumberOne.py
    blah

---
+++ LessonNumberOne.py
@@ -0,0 +1,40 @@
+C:\Users\rawrg>py
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:57:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> raw_input()
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'raw_input' is not defined
+>>> input()
+I am Learning at hackerearth
+'I am Learning at hackerearth'
+>>> print("Guido")
+Guido
+>>> Print('"Guido"')
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'Print' is not defined
+>>> print('"Guido"')
+"Guido"
+>>>>>> beautiful_number = input("tell me a beautiful numer ")
+tell me a beautiful numer 25
+>>> print(beautiful_number)
+25
+>>> print(Beautiful_number)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'Beautiful_number' is not defined
+>>> print("5"*6)
+555555
+>>> print(beautiful_number*6)
+252525252525
+>>> print(5,6,7)
+5 6 7
+>>> print(567)
+567
+>>> print(5 6 7)
+  File "<stdin>", line 1
+    print(5 6 7)
+            ^
+SyntaxError: invalid syntax
+>>>
